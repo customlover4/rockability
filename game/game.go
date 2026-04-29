58ec1a553c870a01204cb6c928b34145a8f43df3
Daniil Ulyanov <smdfuck@icloud.com>
2026-04-29 15:21:53 +0300

    add random field for game struct

--- game/game.go
+++ game/game.go
@@ -3,20 +3,21 @@ package game
 import (
 	"game/game/player"
 	"game/tui"
+	"math/rand/v2"
+	"time"
 )
 
 type Game struct {
 	app *tui.App
-
-	p *player.Player
+	rnd *rand.Rand
+	p   *player.Player
 }
 
 func New(app *tui.App) *Game {
 	return &Game{
 		app: app,
+		rnd: rand.New(rand.NewPCG(uint64(time.Now().UnixNano()), uint64(time.Now().Unix()))),
 
 		p: player.DefaultPlayer(),
 	}
 }
-
-
