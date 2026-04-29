a632d0a51d6e681f3d461b7682a6b67d6a529905
Daniil Ulyanov <smdfuck@icloud.com>
2026-04-29 15:20:41 +0300

    events system, and some player updates

--- game/player/player.go
+++ game/player/player.go
@@ -1,7 +1,10 @@
 package player
 
 import (
+	"container/list"
 	"fmt"
+	"game/game/player/band"
+	"game/game/player/music"
 	"game/game/player/region"
 	"game/game/player/stats"
 	"game/tui"
@@ -14,11 +17,15 @@ type Player struct {
 	Region region.Region
 
 	playerName string
-	groupName  string
 	money      int
 	agetime    time.Time
 
-	Stats stats.Stats
+	Stats           stats.Stats
+	Band            *band.Band
+	ImprovizationXP float64
+	Ideas           []music.Idea
+
+	events *list.List
 }
 
 func DefaultPlayer() *Player {
@@ -26,11 +33,15 @@ func DefaultPlayer() *Player {
 
 	return &Player{
 		playerName: "Безымянный",
-		groupName:  "Безымянный",
 		Region:     region.DefaultRegion(),
 		money:      0,
-		Stats:      stats.DefaultStats(),
 		agetime:    at,
+
+		Stats:           stats.DefaultStats(),
+		ImprovizationXP: 0,
+		Ideas:           make([]music.Idea, 0),
+
+		events: list.New(),
 	}
 }
 
@@ -38,13 +49,33 @@ func (p *Player) RenderStatus() []tui.StatusItem {
 	return []tui.StatusItem{
 		{Label: "Имя", Value: p.playerName},
 		{Label: "Возраст", Value: fmt.Sprintf("%d", p.Stats.Age)},
-		{Label: "Группа", Value: p.groupName},
 		{Label: "Регион", Value: p.Region.Title},
 		{Label: "Деньги", Value: fmt.Sprintf(
-			"%d %s", p.money, p.Region.MoneyFormat,
+			"%d $", p.money,
 		)},
 		{Label: "Дата", Value: p.agetime.Format(time.DateOnly)},
 	}
 }
 
 // todo: save/load functions
+
+func (p *Player) SetName(name string) {
+	p.playerName = name
+}
+
+func (p *Player) SetRegion(region region.Region) {
+	p.Region = region
+}
+
+func (p *Player) AddMoney(value int) bool {
+	if p.money+value < 0 {
+		return false
+	}
+
+	p.money += value
+	return true
+}
+
+func (p *Player) NewEvent(e Event) {
+	p.events.PushBack(e)
+}
